a439f48a09a9e2567876097b444a4fb764348fa3
Prince7195 <vijaydeepak7195@outlook.com>
2020-04-07 12:54:05 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './style.css';
-import { data } from './data';
+import { response } from './data';
 import { Document, Page } from 'react-pdf';
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      numPages: null,
+      pageNumber: 1,
+    }
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
+  pdf() {
+    const byteCharacters = atob(response);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+          byteNumbers[i] = byteCharacters.charCodeAt(i);
+          }
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], {type: 'application/pdf'});
   }
 
   render() {
+    const { pageNumber, numPages } = this.state;
     return (
       <div>
         <h1>View PDF</h1>
-        <div id="pdf-viewer"></div>
+        <Document
+          file={this.pdf()}
+          onLoadSuccess={this.onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
       </div>
     );
   }
